46f8b3a5c91fc98a45a86b7cc6d9674dd971e3ba
The-ZeGOD <nihalgupta.nirmal@gmail.com>
2024-08-07 00:01:03 +0530

    removing env

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import mongoose from "mongoose"
-import authRoutes from "./routes/AuthRoutes.js"
-import contactsRoutes from "./routes/ContactRoutes.js"
-import setupSocket from "./socket.js"
-import messagesRoutes from "./routes/MessagesRoutes.js"
-import channelRoutes from "./routes/ChannelRoutes.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoutes.js";
+import setupSocket from "./socket.js";
+import messagesRoutes from "./routes/MessagesRoutes.js";
+import channelRoutes from "./routes/ChannelRoutes.js";
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -16,11 +16,11 @@ const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
 app.use(
-    cors({
-        origin: [process.env.ORIGIN],
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        credentials: true,
-    })
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
 );
 
 app.use("/uploads/profiles", express.static("uploads/profiles"));
@@ -34,14 +34,18 @@ app.use("/api/contacts", contactsRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/channel", channelRoutes);
 
-const server = app.listen(port, ()=> {
-    console.log(`Server is running at http://localhost:${port}`);
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
 
 setupSocket(server);
 
-mongoose.connect(databaseURL).then(()=>console.log("Database connection is successful."))
+mongoose.connect(databaseURL, {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+    authSource:"admin"
+  }).then(()=>console.log("Database connection is successful."))
 .catch((error) => {
+    console.error(error);
     console.log(error.message);
     console.log("Database connection failed")
 });
